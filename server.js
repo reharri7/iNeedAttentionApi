0705729718f2fbf33615097888a6a031d246c10a
Rhett Harrison <reharri7@asu.edu>
2020-09-17 14:25:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const app = express();
+
+const db = {
+    users :[
+        {
+            id: '1',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+    ]
+}
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -12,6 +25,44 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res)=> {
     res.send('This is working');
+});
+
+app.post('/signin', (req, res) => {
+    if( req.body.email === db.users[0].email && 
+        req.body.password === db.users[0].password){
+            res.json('success');
+        }else{
+            res.status(400).json('error logging in');
+        }
+});
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    db.users.push(
+        {
+            id: db.users.length + 1,
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+        }
+    );
+    res.json(db.users[db.users.length - 1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    db.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if(!found){
+        res.status(400).json('user not found');
+    }
 });
 
 app.listen(3000, () => {
